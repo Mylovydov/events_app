5942dbbceb4f0aac0957815c0ebdfbcc876c52ec
Denys <denis.milovidov@devit.group>
2023-12-10 23:04:07 +0200

    add userId to TOmitEvent

--- client/src/utils/helpers/validateEvents/validateEvents.types.ts
+++ client/src/utils/helpers/validateEvents/validateEvents.types.ts
@@ -2,5 +2,5 @@ import { TEvent } from '@/services';
 
 export type TOmitEvent = Omit<
 	TEvent,
-	'_id' | 'isEmailSend' | 'createdAt' | 'updatedAt'
+	'_id' | 'isEmailSend' | 'createdAt' | 'updatedAt' | 'userId'
 >;
