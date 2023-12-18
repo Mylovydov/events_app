c5e07165aef358c5f4d757e3361771e292342284
Denys <denis.milovidov@devit.group>
2023-12-18 21:29:34 +0200

    fix window error

--- server/src/utils/helpers/prepareValidationError.ts
+++ server/src/utils/helpers/prepareValidationError.ts
@@ -1,11 +1,13 @@
 import { ZodIssue } from 'zod';
 
 export const prepareValidationError = (errors: ZodIssue[]): string => {
-	return errors.map(({ path, message }) => {
-		const objIndex = +path[0] + 1;
+	const errorMessage = errors.map(({ path, message }) => {
+		const objIndex = +(path[0] || 0) + 1;
 		const field = path[1];
 		return `Error ${message.toLowerCase()} in row ${objIndex}: field - ${field}`;
-	})[0];
+	});
+
+	return errorMessage.at(0) || 'Validation error';
 };
 
 export default prepareValidationError;
